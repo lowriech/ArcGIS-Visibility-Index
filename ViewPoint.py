662ac97bc63e5085393cc085ccd11dde5890e1d6
Chris Lowrie <lowriech@msu.edu>
2017-02-19 15:01:18 -0800

    Update ViewPoint.py

--- ViewPoint.py
+++ ViewPoint.py
@@ -2,18 +2,17 @@ import numpy
 import arcpy
 
 class ViewPoint:
-
 	def __init__(self, FID, X, Y, Z, viewshed):
 		self.FID = FID
 		self.x = X
 		self.y = Y
-		self.z = Z
+		self.z = Z + 2
 		self.viewshed =  viewshed + "\\poly_" + str(FID) +".shp"
 		self.total = 0.0
 		self.visibleCells = []
 
-	def setVisibleCells(self, list):
-		self.visibleCells = list
+	def setVisibleCells(self, cells):
+		self.visibleCells = cells
 
 	def setTotal(self, number):
 		self.total = number
